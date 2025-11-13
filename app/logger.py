8dc305d0e0f5525549ea02c1c2c1502c12dd63be
cuhrmunken <elliott.davis@crestonetech.com>
2025-11-13 11:03:14 -0700

    created logging

--- app/logger.py
+++ app/logger.py
@@ -1,11 +1,12 @@
 import logging
 import logging.config
+
 from typing import Optional
 
 
 LOG_FORMAT = "[%(asctime)s] [%(levelname)s] [%(name)s] [%(filename)s:%(lineno)d] - %(message)s"
 
-def setup_logging(log_level: str = "INFO", log_file: Optional[str] = "./logs/app.log"):
+def setup_logging(log_level: str = "INFO", log_file: Optional[str] = "logs/app.log"):
     logging_config = {
         "version": 1,
         "disable_existing_loggers": False,
@@ -21,18 +22,13 @@ def setup_logging(log_level: str = "INFO", log_file: Optional[str] = "./logs/app
                 "formatter": "standard",
                 
             },
-            "bloofo": {
-                "class": "logging.StreamHandler",
-                "level": log_level,
-                "formatter": "standard",
-            },
             "file": {
-                "class": "logging.FileHandler",
+                "class": "logging.handlers.RotatingFileHandler",
                 "level": log_level,
                 "formatter": "standard",
                 "filename": log_file,
- #               "maxBytes": 1024 * 1024 * 5, # 5MB
-#                "backupCount": 5,
+                "maxBytes": 1024 * 1024 * 5, # 5MB  
+                "backupCount": 5, # keep 5 backup files
                 "encoding": "utf-8"
             },
         },
