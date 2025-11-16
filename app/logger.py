93129fa55a147e2e5f7f92acf9188c8f494eee7e
cuhrmunken <elliott.davis@crestonetech.com>
2025-11-15 17:12:02 -0700

    adds brief log format

--- app/logger.py
+++ app/logger.py
@@ -5,6 +5,7 @@ from typing import Optional
 
 
 LOG_FORMAT = "[%(asctime)s] [%(levelname)s] [%(name)s] [%(filename)s:%(lineno)d] - %(message)s"
+LOG_FORMAT_BRIEF = "[%(levelname)s] [%(name)s] [%(filename)s:%(lineno)d] - %(message)s"
 
 def setup_logging(log_level: str = "INFO", log_file: Optional[str] = "logs/app.log"):
     logging_config = {
@@ -13,13 +14,16 @@ def setup_logging(log_level: str = "INFO", log_file: Optional[str] = "logs/app.l
         "formatters": {
             "standard": {
                 "format": LOG_FORMAT
+            },
+            "brief": {
+                "format": LOG_FORMAT_BRIEF
             }
         },
         "handlers": {
             "console": {
                 "class": "logging.StreamHandler",
                 "level": log_level,
-                "formatter": "standard",
+                "formatter": "brief",
                 
             },
             "file": {
