0bb08e02e158df2edcfa9b08b516a8361ce00f5b
cuhrmunken <elliott.davis@crestonetech.com>
2025-11-28 23:17:45 -0700

    add validate_root_and_output_directory

--- gcrs/api/utils.py
+++ gcrs/api/utils.py
@@ -2,6 +2,7 @@
 from pathlib import Path
 from datetime import datetime
 from typing import Literal
+from fastapi import HTTPException, status
 
 # local imports
 from gcrs.logger import setup_logging
@@ -38,6 +39,32 @@ def validate_directory_path(path: str) -> Path | None:
     except (OSError, ValueError) as e:
         logger.error("Error validating path %s: %s", path, e)
         return None
+
+def validate_root_and_output_directory(repo_root: str) -> tuple[Path, Path]:
+    """Validate the repository root directory and create the output directory if it doesn't exist.
+
+    Args:
+        repo_root: Path to the repository root directory.
+    Raises:
+        HTTPException: If the repository root directory does not exist or is not a directory.
+    Returns:
+        A tuple containing the repository root directory and the output directory.
+        None if the repository root directory does not exist or is not a directory.
+    """
+    repo_root_path = validate_directory_path(repo_root)
+    if repo_root_path is None:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail={
+                "status": "error",
+                "error": f"The specified repository root directory [{repo_root}] does not exist or is not a directory",
+            },
+        )
+
+    output_dir_path = repo_root_path / './output'
+    output_dir_path.mkdir(parents=True, exist_ok=True)
+
+    return (repo_root_path, output_dir_path)
 
 def generate_output_filename(repo_root: str, operation: Literal["scan", "summary"] = "scan", output_file_format: Literal["json", "markdown", "csv"] = "json") -> str:
     """Generate a default output filename with repo name and timestamp.
